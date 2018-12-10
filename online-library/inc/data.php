f7b6d4b699c1776b7e331d55d8f9f84701b2f4d4
Cesartorres12 <cesartorres726@gmail.com>
2018-12-10 18:27:26 -0500

    Add files via upload

---
+++ online-library/inc/data.php
@@ -0,0 +1,168 @@
+<?php
+$catalog = [];
+//Books
+$catalog[101] = [
+	"title" => "Four",
+	"img" => "img/media/four.jpg",
+    "genre" => "Biography",
+    "format" => "Paperback",
+    "year" => 2007,
+    "category" => "Books",
+    "authors" => [
+        "Scott Galloway"
+    ],
+    "publisher" => "Penguin Publishing Group",
+    "isbn" => '9780735213678'
+];
+$catalog[102] = [
+    "title" => "Leaders Eat Last",
+    "img" => "img/media/leaderseatlast.png",
+    "genre" => "Business + Money Management, Psychology",
+    "format" => "Paperback",
+    "year" => 2014,
+    "category" => "Books",
+    "authors" => [
+        "Simon Sinek"
+    ],
+    "publisher" => "Penguin Group USA",
+    "isbn" => '978-0132350884'
+];
+$catalog[103] = [
+    "title" => "Start With Why",
+    "img" => "img/media/startwithwhy.png",
+    "genre" => " Business",
+    "format" => "Hardcover",
+    "year" => 2009,
+    "category" => "Books",
+    "authors" => [
+        "Simon Sinek"
+    ],
+    "publisher" => "Penguin Publishing Group",
+    "isbn" => '9780241958223'
+];
+$catalog[104] = [
+    "title" => "Creativity Inc.",
+    "img" => "img/media/creativity.png",
+    "genre" => "Business, Economics",
+    "format" => "Hardcover",
+    "year" => 2014,
+    "category" => "Books",
+    "authors" => [
+        "Ed Catmull"
+    ],
+    "publisher" => "Random House Publishing Group",
+    "isbn" => '007-6092046981'
+];
+//Movies
+$catalog[201] = [
+    "title" => "Back to The Future",
+    "img" => "img/media/back2thefuture.jpg",
+    "genre" =>
+		"Adventure",
+		"Comedy",
+		"Sci-fi",
+    "format" => "DVD",
+    "year" => 1985,
+    "category" => "Movies",
+    "director" => "Robert Zemeckis",
+    "writers" => [
+        "Robert Zemeckis",
+        "Bob Gale"
+    ],
+    "stars" => [
+        "Micheal J Fox",
+        "Christopher Lloyd",
+        "Lea Thompson",
+    ]
+];
+$catalog[202] = [
+    "title" => "The Lion King",
+    "img" => "img/media/thelionking.jpg",
+    "genre" => "Animation, Drama, Adventure",
+    "format" => "Blu-ray",
+    "year" => 1994,
+    "category" => "Movies",
+    "director" => " Roger Allers, Rob Minkoff",
+    "writers" => [
+        "Irene Mecchi (screenplay by), Jonathan Roberts"
+    ],
+    "stars" => [
+        "Matthew Broderick",
+        "Jeremy Irons",
+        "James Earl Jones ",
+    ]
+];
+$catalog[203] = [
+    "title" => "Malcom X",
+    "img" => "img/media/malcomx.jpg",
+    "genre" => "Biography, Drama, History ",
+    "format" => "DVD",
+    "year" => 1992,
+    "category" => "Movies",
+    "director" => "Spike Lee",
+    "writers" => [
+        " Alex Haley (book)",
+        " Malcolm X (book)"
+
+    ],
+    "stars" => [
+        "Denzel Washington",
+        "Angela Bassett",
+        "Delroy Lindo "
+
+    ]
+];
+$catalog[204] = [
+    "title" => "The Godfather",
+    "img" => "img/media/thegodfather.jpg",
+    "genre" => "Drama, Crime",
+    "format" => "DVD",
+    "year" => 1972,
+    "category" => "Movies",
+    "director" => "Francis Ford Coppola",
+    "writers" => [
+        "ario Puzo (screenplay by), Francis Ford Coppola (screenplay by)"
+    ],
+    "stars" => [
+        "Marlon Brando",
+        "Al Pacino",
+        "James Caan"
+    ]
+];
+//Music
+$catalog[301] = [
+    "title" => "Forest Hills",
+    "img" => "img/media/Jcole.jpg",
+    "genre" => "Hip-Hop, Rap",
+    "format" => "CD",
+    "year" => 2014,
+    "category" => "Music",
+    "artist" => "Jcole"
+];
+$catalog[302] = [
+    "title" => "Thriller",
+    "img" => "img/media/thriller.jpg",
+    "genre" => "Funk / Soul, Pop",
+    "format" => "Vinyl",
+    "year" => 1982,
+    "category" => "Music",
+    "artist" => "Micheal Jackson"
+];
+$catalog[303] = [
+    "title" => "The Infamous",
+    "img" => "img/media/mobdeepp.jpg",
+    "genre" => "Rap",
+    "format" => "CD",
+    "year" => 1995,
+    "category" => "Music",
+    "artist" => "Mobb Deep"
+];
+$catalog[304] = [
+    "title" => "Cloring Book",
+    "img" => "img/media/chancetherapper.jpg",
+    "genre" => "Hip-hop, Rap",
+    "format" => "iTunes",
+    "year" => 2016,
+    "category" => "Music",
+    "artist" => "Chance the Rapper"
+];
